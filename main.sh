c9c676210ae1f037a5bb2938e59afe80b53fbb98
Basu96 <basu.96@gmail.com>
2017-01-09 23:46:37 +0530

    another commit

--- main.sh
+++ main.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
+echo "installation"
 
+pip install PyAudio
+pip install pydub
+pip install ffmpeg
+pip install matplotlib
+pip install numpy
+pip install scipy
+sudo apt install mysql-client
 
-echo "sadasd"
+pip install virtualenv
+virtualenv --system-site-packages env_with_system
+
+pip install pydejavu
+
