c2cf330fd9825f8cdb099d84f42a2a54d4ba9e2a
Basu96 <basu.96@gmail.com>
2017-01-09 23:16:25 +0530

    Second commit

--- main.sh
+++ main.sh
@@ -1,21 +1,4 @@
 #!/bin/bash
-echo "NAMASTE"
 
 
-#installing ffmpeg libraries for chromaprint. This may take few minutes
-cd ffmpeg-3.2.2
-tar xvjf ffmpeg-3.2.2.tar.bz2
-./configure --disable-yasm --enable-shared
-sudo make
-sudo make install
-cd ..
-
-#installing chromaprint
-cd chromaprint-1.4
-cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_TOOLS=ON
-make
-sudo make install
-cd ..
-#installation complete
-
-echo "INSTALLATION COMPLETE"
+echo "sadasd"
